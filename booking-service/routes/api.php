0290102afbb240afc97a8c98a901a0c3906182bf
Nailatul <nailatuliftori@student.telkomuniversity.ac.id>
2026-04-27 09:08:32 +0700

    add booking-service

---
+++ booking-service/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\BookingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/bookings', [BookingController::class, 'index']);
+Route::get('/bookings/history/{userId}', [BookingController::class, 'historyByUser']);
+Route::post('/bookings', [BookingController::class, 'store']);
